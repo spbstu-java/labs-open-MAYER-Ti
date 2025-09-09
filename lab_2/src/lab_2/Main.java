539d6ccfc129ceeef742df613225e25084cb6b72
MAYER-Ti <s.lutov@inbox.ru>
2025-09-09 22:56:08 +0300

    Вторая лабораторная

---
+++ lab_2/src/lab_2/Main.java
@@ -0,0 +1,69 @@
+package lab_2;
+
+import java.lang.reflect.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Class<?> clazz = DemoService.class;
+        Object instance = createInstance(clazz);
+
+        for (Method m : clazz.getDeclaredMethods()) {
+            if (!m.isAnnotationPresent(Repeat.class)) {
+                continue;
+            }
+
+            int mods = m.getModifiers();
+            if (!(Modifier.isProtected(mods) || Modifier.isPrivate(mods))) {
+                continue;
+            }
+
+            int times = m.getAnnotation(Repeat.class).value();
+            m.setAccessible(true);
+            Object[] params = buildDefaultArgs(m.getParameterTypes());
+            Object target = Modifier.isStatic(mods) ? null : instance;
+
+            for (int i = 0; i < times; i++) {
+                Object result = m.invoke(target, params);
+                if (m.getReturnType() != void.class) {
+                    System.out.println("Result of " + m.getName() + " #" + (i + 1) + ": " + result);
+                }
+            }
+        }
+    }
+
+    private static Object createInstance(Class<?> clazz) throws Exception {
+        Constructor<?> ctor = clazz.getDeclaredConstructor();
+        ctor.setAccessible(true);
+        return ctor.newInstance();
+    }
+
+    private static Object[] buildDefaultArgs(Class<?>[] types) {
+        Object[] args = new Object[types.length];
+        for (int i = 0; i < types.length; i++) {
+            args[i] = defaultValue(types[i]);
+        }
+        return args;
+    }
+
+    private static Object defaultValue(Class<?> type) {
+        if (!type.isPrimitive()) {
+            if (type == String.class) return "";
+            if (type.isArray()) return Array.newInstance(type.getComponentType(), 0);
+            if (type.isEnum()) {
+                Object[] constants = type.getEnumConstants();
+                return (constants != null && constants.length > 0) ? constants[0] : null;
+            }
+            return null;
+        }
+        if (type == boolean.class) return false;
+        if (type == byte.class)    return (byte) 0;
+        if (type == short.class)   return (short) 0;
+        if (type == int.class)     return 0;
+        if (type == long.class)    return 0L;
+        if (type == float.class)   return 0f;
+        if (type == double.class)  return 0d;
+        if (type == char.class)    return '\0';
+        return null;
+    }
+}
