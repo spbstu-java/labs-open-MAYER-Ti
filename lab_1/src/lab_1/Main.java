1cea5f1f057de5efafb5a32bc30ed6f8c984140d
MAYER-Ti <s.lutov@inbox.ru>
2025-09-09 21:46:34 +0300

    Добавлено взаимодействие с пользователем

--- lab_1/src/lab_1/Main.java
+++ lab_1/src/lab_1/Main.java
@@ -1,4 +1,5 @@
 package lab_1;
+import java.util.Scanner;
 
 public class Main {
 
@@ -6,17 +7,44 @@ public class Main {
         Point saintPetepsburg = new Point(59.9386, 30.3141);
         Point moscow = new Point(55.7522, 37.6156);
         Point minsk = new Point(53.9, 27.5667);
+        Point cities[] = {saintPetepsburg, moscow, minsk};
 
         HorseMovement horse = new HorseMovement();
         LegsMovement legs = new LegsMovement();
 
         Hero aleshaPopovich = new Hero(legs, minsk);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Alesha Popovich dislocate in " + aleshaPopovich.getDislocation().string());
-        aleshaPopovich.move(moscow);
-        aleshaPopovich.setMovement(horse);
-        System.out.println("Alesha Popovich dislocate in " + aleshaPopovich.getDislocation().string());
-        aleshaPopovich.move(saintPetepsburg);
-        System.out.println("Alesha Popovich dislocate in " + aleshaPopovich.getDislocation().string());
+        int choise = 1;
+        int indexCity = 0;
+        while (true) {
+            System.out.println("Алёша Попович находится в " + aleshaPopovich.getDislocation().string());
+            System.out.println("Введите каким способом Алёша Попович будет добираться до цели:\n" +
+                    "1) Пешем\n" +
+                    "2) На осле\n" +
+                    "3) Закончить");
+            while (!scanner.hasNextInt()) {
+                System.out.print("Это не число! Попробуйте заново: ");
+                scanner.next();
+            }
+            choise = scanner.nextInt();
+            switch (choise) {
+                case 1:
+                    aleshaPopovich.setMovement(legs);
+                    break;
+                case 2:
+                    aleshaPopovich.setMovement(horse);
+                    break;
+                case 3:
+                    // Выйти из цикла программы
+                    return;
+                default:
+                    System.out.println("Некорректный вариант");
+                    break;
+            }
+            if ((indexCity+1) < (int)cities.length) {
+                aleshaPopovich.move(cities[++indexCity]);
+            }
+        }
     }
 }
